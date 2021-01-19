435c777daa33bc612919863525ab227fed1289bd
Atoullo Boboev <atoullo.boboev@gmail.com>
2021-01-19 11:50:01 -0500

    Second exercise.

---
+++ src/com/codewithatoullo/DemoExercise.java
@@ -0,0 +1,11 @@
+package com.codewithatoullo;
+
+public class DemoExercise {
+    public static void main(String[] args) {
+        System.out.print("От");
+        for (int i=10; i<100; i+=2){
+            System.out.print(" " + i );
+        }
+        System.out.print(" дo.");
+    }
+}
